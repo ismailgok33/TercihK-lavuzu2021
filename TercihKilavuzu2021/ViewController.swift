5b88c9c34c8c4bcdd4fc869a54838ca42bdecada
İsmail <ismailgok33@gmail.com>
2021-08-04 20:31:03 +0300

    Initial Commit

---
+++ TercihKilavuzu2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TercihKilavuzu2021
+//
+//  Created by İsmail on 4.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
